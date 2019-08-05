c972acfec3ed4ebaf89b9f89215126520e0037d9
Rod20 <nimrod2560@gmail.com>
2019-08-05 16:51:14 -0400

    Implementando BottomNavigation

--- lib/trips.dart
+++ lib/trips.dart
@@ -8,6 +8,29 @@ class Trips extends StatefulWidget {
 class _TripsState extends State<Trips> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.white,
+            primaryColor: Colors.purple
+          ),
+          child: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  title: Text("")
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    title: Text("")
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    title: Text("")
+                )
+              ]
+          )
+      ),
+    );
   }
 }
